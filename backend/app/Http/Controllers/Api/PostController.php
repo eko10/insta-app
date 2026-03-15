48a4262104b109b93b9530739e40e46b497dd1ae
eko10 <eko3227@gmail.com>
2026-03-15 23:37:22 +0700

    feat: add method post/store

--- backend/app/Http/Controllers/Api/PostController.php
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
         return Post::with('user', 'likes', 'comments')->latest()->paginate(10);
+    }
+
+    public function store(Request $request)
+    {
+        $path = $request->file('image')->store('posts', 'public');
+
+        $post = Post::create([
+            'user_id' => auth()->id(),
+            'caption' => $request->caption,
+            'image' => $path
+        ]);
+
+        return response()->json($post);
     }
 }
