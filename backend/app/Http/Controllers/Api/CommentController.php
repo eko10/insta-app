d20718c5b7daf8ecc45719b34868fca62c9c593d
eko10 <eko3227@gmail.com>
2026-03-16 00:22:46 +0700

    feat: add method comment

--- backend/app/Http/Controllers/Api/CommentController.php
+++ backend/app/Http/Controllers/Api/CommentController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, $postId)
+    {
+        return Comment::create([
+            'user_id' => auth()->id(),
+            'post_id' => $postId,
+            'comment' => $request->comment
+        ]);
+
+        return response()->json(['message' => 'saved comment']);
+    }
 }
