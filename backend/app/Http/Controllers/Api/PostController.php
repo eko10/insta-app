2519e9bfe974e9458d65ea6ec2b049d11dac7cf0
eko10 <eko3227@gmail.com>
2026-03-15 23:30:36 +0700

    feat: add method post/index

--- backend/app/Http/Controllers/Api/PostController.php
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return Post::with('user', 'likes', 'comments')->latest()->paginate(10);
+    }
 }
