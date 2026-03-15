89f38acc1059ea229984c4cd5b65f3fbe77d9a9f
eko10 <eko3227@gmail.com>
2026-03-15 23:05:30 +0700

    fix: delete unused code in models

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Models\Comment;
 use App\Models\Like;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
