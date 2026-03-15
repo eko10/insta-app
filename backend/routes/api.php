14a9f445656db7dbb6f62f3b52d8573b7fa19ae8
eko10 <eko3227@gmail.com>
2026-03-15 23:37:56 +0700

    feat: add store post route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,4 +20,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/posts', [PostController::class, 'store']);
 });
